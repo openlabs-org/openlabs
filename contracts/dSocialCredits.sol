9562ef01bfb3d83422951652d3c7c87a3ffb2a27
Kuan <kuanjh123@gmail.com>
2021-08-05 14:39:45 -0700

    reactjs + truffle + smart contracts

---
+++ contracts/dSocialCredits.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.3; 
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+
+contract dSocialCredits is ERC1155 {
+
+    address _admin;
+
+    constructor(address _adm) ERC1155("") {
+        _admin = _adm;
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        require(false, "Transfer disabled");
+        _safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override {
+        require(false, "Transfer disabled");
+        _safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    function burn(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) public {
+        require(msg.sender == _admin, "Only designated admin can mint.");
+        _burn(account, id, amount);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public {
+        require(msg.sender == _admin, "Only designated admin can mint.");
+        _mint(account, id, amount, data);
+    }
+
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public {
+        require(msg.sender == _admin, "Only designated admin can mint.");
+        _mintBatch(to, ids, amounts, data);
+    }
+
+}
---
+++ contracts/desilo.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.8.3; 
+
+import "./dSocialCredits.sol";
+import "./library/ABDKMath64x64.sol";
+
+contract desilo {
+
+    uint256 _groupCreationSCAmount;
+    uint256 _scStakeAmount;
+    dSocialCredits _scContract; 
+    address _owner;
+    uint256 _scID = 0;
+
+    // mapping(entityID)
+    mapping(uint256 => mapping(address => uint256)) _stakedAmount; 
+    mapping(uint256 => mapping(address => uint256)) _stakeExpiry; 
+    mapping(uint256 => uint256) _entityToThread;
+    mapping(uint256 => string) _entityURI;
+
+    // mapping(threadID)
+    mapping(uint256 => string) _threadURI;
+    mapping(uint256 => mapping(uint256 => bool)) _threadGSC;
+    mapping(uint256 => uint256) _threadGSCCount;
+
+
+    // mapping(groupID => fixed128x128)
+    uint _groupCount = 1; 
+    mapping(uint256 => string) _groupURI;
+    mapping(uint256 => int128) _gscYield;
+    mapping(uint256 => uint256) _gscMinAcceptance; 
+
+
+    constructor(uint256 scStakeAmount, int128 scYield, uint256 groupCreationSCAmount) {
+        _gscYield[0] = scYield;
+        _scStakeAmount = scStakeAmount;
+        _groupCreationSCAmount = groupCreationSCAmount;
+        _owner = msg.sender;
+    }
+
+    function createGroup(uint256 _initialSupply, string memory _uri) external {
+        _scContract.burn(msg.sender, _scID, _groupCreationSCAmount);
+        _groupURI[_groupCount] = _uri; 
+        _scContract.mint(msg.sender, _groupCount, _initialSupply, "");
+        _groupCount++;
+    }
+
+    function setSC(dSocialCredits _sc) external {
+        require(msg.sender == _owner);
+        _scContract = _sc;
+    }
+
+    function getStaked(uint256 _commitId, address _addr) public view returns(uint256) {
+        return _stakedAmount[_commitId][_addr];
+    }
+
+    function stake(uint256 _commitId) external {
+        _scContract.safeTransferFrom(msg.sender, address(this), _scID, _scStakeAmount, "");
+        _stakedAmount[_commitId][msg.sender] += _scStakeAmount;
+    }
+
+    function unstake(uint256 _commitId) external {
+        require(block.timestamp >= _stakeExpiry[_commitId][msg.sender], "desilo: You may not yet unstake."); 
+        require(getStaked(_commitId,msg.sender) >= 0, "desilo: You have not yet staked."); 
+        
+        uint256 stakedAmount = getStaked(_commitId,msg.sender);
+        uint256 threadID = _entityToThread[_commitId];
+        uint256 gscCount = _threadGSCCount[threadID];
+        uint256[] memory ids = new uint256[](gscCount);
+        uint256[] memory amounts = new uint256[](gscCount);
+
+
+        _stakedAmount[_commitId][msg.sender] -= stakedAmount; 
+        _stakeExpiry[_commitId][msg.sender] = 0;
+
+        _scContract.safeTransferFrom(address(this), msg.sender, _scID, stakedAmount, "");
+        _scContract.mint(msg.sender, _scID, ABDKMath64x64.mulu(_gscYield[0], stakedAmount), "");
+
+        uint256 count = 0;
+        for (uint256 i = 1; i < _groupCount; i++) {
+            if (_threadGSC[threadID][i]) {
+                ids[count] = i;
+                amounts[count] = ABDKMath64x64.mulu(_gscYield[i], stakedAmount);
+                count++;
+            }
+        }
+        _scContract.mintBatch(msg.sender, ids, amounts, "");
+    }
+
+}
