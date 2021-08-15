e0c1934cf5200fb94fc15cba0f3696c1ca531afb
Alexis JC <alexisjc.dev@gmail.com>
2021-08-15 17:02:13 +0200

    feat: improve profile page

--- client/src/views/Profile.js
+++ client/src/views/Profile.js
@@ -13,11 +13,13 @@ import {
   Grid,
   Card,
   CardMedia,
-  CardContent
+  CardContent,
+  Typography
 } from "@material-ui/core";
 import React, { useContext, useEffect, useState } from "react";
 import { IDX } from "@ceramicstudio/idx";
 import { fetch as fetchProfile } from "../api/ProfileRepository";
+import { fetchAll as fetchProjects } from "../api/ProjectRepository";
 
 import UserContext from "../context/UserContext";
 import { useHistory, useParams } from "react-router-dom";
@@ -27,6 +29,7 @@ import ProjectForm from "../components/Project/ProjectForm";
 import GroupForm from "../components/Project/GroupForm";
 import { storeFiles } from "../api/Web3storage";
 import { Skeleton } from "@material-ui/lab";
+import ProjectCard from "../components/Project/ProjectCard";
 
 const useStyles = makeStyles((theme) => ({
   folderList: {
@@ -51,6 +54,7 @@ export default function Profile() {
   const history = useHistory();
   const { ceramic, idx, desiloContract } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(false);
+  const [projects, setProjects] = useState([]);
 
   const handleProfileUpdate = async ({name, description, dropZoneFile}) => {
     let newPictureCid = null;
@@ -88,6 +92,8 @@ export default function Profile() {
         const records = await idx.get("basicProfile", id);
         if (records) setRecords(records);
         setProfile(await fetchProfile({ idx, ceramic, desiloContract }, id));
+        const projects = await fetchProjects({ idx, ceramic, desiloContract })
+        setProjects(projects.filter((project) => true))
         setIsLoading(false);
       } catch (error) {
         console.warn(error);
@@ -102,13 +108,12 @@ export default function Profile() {
     <Grid container spacing={3}>
       <Grid item xs={4}>
         <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <h1>Profile</h1>
-          </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={3}>
             <Card>
-              <CardMedia
-              style={{height: "300px"}}
+
+              <CardMedia 
+                
+                style={{height: "75px"}}
                 image={
                   records.url
                     ? records.url
@@ -116,70 +121,20 @@ export default function Profile() {
                 }
               />
             </Card>
-            <CardContent>
-              <TextField
-                variant="outlined"
-                placeholder="enter name..."
-                value={records.name}
-                disabled
-                style={{width: "100%"}}
-              ></TextField>
-            </CardContent>
-            <CardContent>
-              <TextField
-                variant="outlined"
-                placeholder="enter affiliation..."
-                value={records.description}
-                disabled
-                style={{width: "100%"}}
-              ></TextField>
-            </CardContent>
           </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={8}>
-        <Grid container spacing={3}>
-          {idx.authenticated && id == idx.id && (
-            <>
-              <Grid item>
-                <Button variant="contained" color="primary" onClick={() => setProfileDialog(true)} startIcon={<Edit/>}>
-                  Edit my profile
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" color="primary" onClick={() => setProjectDialog(true)} startIcon={<Add/>}>
-                  Create a project
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" color="primary" onClick={() => setGroupDialog(true)} startIcon={<Add/>}>
-                  Create a lab
-                </Button>
-              </Grid>
-              {profile.socialCredits && profile.socialCredits[profile.socialCredits.length - 1].amountLifetime == 0 && (
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => {
-                      (async () => {
-                        await desiloContract.methods.seedSC().send();
-                        await loadPage();
-                      })();
-                    }}
-                    startIcon={<Loyalty/>}
-                  >
-                    Get Starting Credits
-                  </Button>
-                </Grid>
-              )}
-            </>
-          )}
+          <Grid item xs={9}>
+            <Typography variant="h3">{records.name}</Typography>
+            <Typography variant="subtitle-1">from {records.description}</Typography>
+          </Grid>
           <Grid item xs={12}>
-            <h1>Social Credits</h1>
+            <Typography variant="subtitle1">Social Credits</Typography>
             <List className={classes.folderList}>
               {isLoading ?
-                <Skeleton animation="wave"/>
+              <>
+                <Skeleton animation="wave" width="100%" height={50} />
+                <Skeleton animation="wave" width="100%" height={50} />
+                <Skeleton animation="wave" width="100%" height={50} />
+              </>
               : 
               profile.socialCredits
                 ? profile.socialCredits.map((sc, index) =>
@@ -217,11 +172,63 @@ export default function Profile() {
                 : ""}
             </List>
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={8}>
+        <Grid container spacing={3}>
+          {idx.authenticated && id == idx.id && (
+            <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => setProfileDialog(true)} startIcon={<Edit/>}>
+                    Edit my profile
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => setProjectDialog(true)} startIcon={<Add/>}>
+                    Create a project
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="primary" onClick={() => setGroupDialog(true)} startIcon={<Add/>}>
+                    Create a lab
+                  </Button>
+                </Grid>
+                {profile.socialCredits && profile.socialCredits[profile.socialCredits.length - 1].amountLifetime == 0 && (
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => {
+                        (async () => {
+                          await desiloContract.methods.seedSC().send();
+                          await loadPage();
+                        })();
+                      }}
+                      startIcon={<Loyalty/>}
+                    >
+                      Get Starting Credits
+                    </Button>
+                  </Grid>
+                )}
+              </Grid>
+            </Grid>
+          )}
           <Grid item xs={12}>
-              
-          </Grid>
-          <Grid item xs={12}>
-
+            <Typography variant="subtitle1">Projects</Typography>
+            <Grid container spacing={3}>
+            {isLoading ? 
+              Array.from(Array(3)).map((_, index) => (
+                <Grid item xs={12} key={"skeleton_project_" + index}>
+                  <ProjectCard project={null} skeleton />
+                </Grid>
+              ))
+            : projects.map(project => (
+              <Grid item xs={12}>
+                <ProjectCard project={project}/>
+              </Grid>
+            ))}
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
